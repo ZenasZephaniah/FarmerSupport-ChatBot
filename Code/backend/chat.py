876444eca10bbdeb7a6210924f2de51e9b31f410
ZenasZephaniah <156357502+ZenasZephaniah@users.noreply.github.com>
2025-08-05 13:27:40 +0530

    Update chat.py

--- Code/backend/chat.py
+++ Code/backend/chat.py
@@ -73,8 +73,12 @@ def chatbotResponse(msg, source_language):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.static_folder = 'static'
-socketio = SocketIO(app, cors_allowed_origins="*")
-
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+# Creating the flask app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.static_folder = 'static'
+socketio = SocketIO(app, cors_allowed_origins="*")  # <-- THIS LINE
 # Creating the socket connection
 @socketio.on('message')
 def handle_message(data):
@@ -86,3 +90,4 @@ def handle_message(data):
 # Running the app
 if __name__ == "__main__":
     socketio.run(app, debug=True)
+
